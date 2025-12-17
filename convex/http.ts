69fefe89881b8b99a54f1574a42382ee46f49f14
sakshis1904 <150705225+sakshis1904@users.noreply.github.com>
2025-12-17 23:27:22 +0530

    updated

--- convex/http.ts
+++ convex/http.ts
@@ -3,8 +3,11 @@ import {WebhookEvent} from "@clerk/nextjs/server";
 import { Webhook } from "svix";
 import {api} from "./_generated/api";
 import { httpAction } from "./_generated/server";
+import {GoogleGenerativeAI} from "@google/generative-ai"
 
 const http = httpRouter()
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 http.route({
     path: "/clerk-webhook",
